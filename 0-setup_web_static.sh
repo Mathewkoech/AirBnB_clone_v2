7f032e6279da19e6afa79c9ae674c4743ef663e3
mathewkoech <mathewkoech55@gmail.com>
2024-05-03 12:45:14 -0700

    Bash script that sets up your web servers for the deployment of web_static

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -26,7 +26,7 @@ sudo echo "<html>
 sudo ln -s  /data/web_static/releases/test/ /data/web_static/current
 
 # Set ownership recursively
-sudo chown -R ubuntu:ubuntu /data/
+sudo chown -R ubuntu: /data/
 
 #update nginx configuration
 sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
