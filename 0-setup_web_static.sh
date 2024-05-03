3d3b1aa361ca56eefd5fd6aaec0f28ebfc20ca6e
mathewkoech <mathewkoech55@gmail.com>
2024-05-03 09:23:48 -0700

    Bash script that sets up your web servers for the deployment of web_static

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Install Nginxif does not exist
+if ! which nginx >/dev/null 2>&1; then
+  sudo apt-get update
+  sudo apt-get install -y nginx
+fi
+sudo ufw allow 'Nginx HTTP'
+# Create directories
+sudo mkdir -p /data/
+sudo mkdir -p /data/web_static/
+sudo mkdir -p /data/web_static/releases/
+sudo mkdir -p /data/web_static/shared/
+sudo mkdir -p /data/web_static/releases/test/
+sudo touch /data/web_static/releases/test/index.html
+
+#creating fake index.html
+sudo echo "<html>
+  <head>
+  </head>
+  <body>
+    <h1>Holberton School</h1>
+  </body>
+</html>" | sudo tee /data/web_static/releases/test/index.html
+
+# Create and update symbolic link
+sudo ln -s  /data/web_static/releases/test/ /data/web_static/current
+
+# Set ownership recursively
+sudo chown -R ubuntu:ubuntu /data/
+
+#update nginx configuration
+sudo sed -i '/listen 80 default_server/a location /hbnb_static { alias /data/web_static/current/;}' /etc/nginx/sites-enabled/default
+
+#restart nginx
+sudo service nginx restart
+
+echo "Web server setup complete!"
