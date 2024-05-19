d9a0a281f608bd696fcbd025556b012c2253cf61
mathewkoech <mathewkoech55@gmail.com>
2024-05-19 11:21:10 -0700

    python route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -38,6 +38,7 @@ def show_text(text):
     text = text.replace('_', ' ')
     return f"C {text}"
 
+
 @app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python_text(text):
